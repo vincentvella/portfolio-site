02a3adbaa7586accc626df7c7c235b77fdb0ba29
vincentvella <vincevella0@gmail.com>
2024-04-07 03:55:57 -0400

    test: add redirects and remove vercel.json

--- next.config.js
+++ next.config.js
@@ -1,20 +1,44 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 /** @type {import('next/dist/next-server/server/config').NextConfig} */
 module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.ctfassets.net",
-        port: "",
-        pathname: "/06126mmge7q6/**",
-      },
-      {
-        protocol: "https",
-        hostname: "asset.brandfetch.io",
-        port: "",
-        pathname: "/idO5iUBn-d/**",
-      },
-    ],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.ctfassets.net",
+				port: "",
+				pathname: "/06126mmge7q6/**",
+			},
+			{
+				protocol: "https",
+				hostname: "asset.brandfetch.io",
+				port: "",
+				pathname: "/idO5iUBn-d/**",
+			},
+		],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "resume.vincevella.com"
+					}
+				],
+				destination: "/resume/:path*"
+			},
+			{
+				source: "/:path*",
+				has: [
+					{
+						type: "host",
+						value: "resume.vincentvella.me"
+					}
+				],
+				destination: "/resume/:path*"
+			}
+		]
+	}
 };
